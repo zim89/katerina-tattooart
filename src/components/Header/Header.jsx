238d1fccae659a5f95cf3d58dcbaa1c66891f299
zim89 <121241841+zim89@users.noreply.github.com>
2023-08-30 21:20:21 +0300

    edit twconfig

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
+import css from './styles.module.css';
+
 const Header = () => {
   return (
-    <div className="section">
-      <div className="container">Header</div>
-    </div>);
+    <div className='section'>
+      <div className='container'>
+        Header
+        <p className={css.text}>sdgdsfbgdfbdfb</p>
+      </div>
+    </div>
+  );
 };
 export default Header;
