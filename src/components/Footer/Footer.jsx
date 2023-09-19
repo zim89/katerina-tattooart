ddd56aed4d7b7022f4172f5e456ea05c28fbb7f7
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-19 12:14:22 +0200

    Moved all section components to modules folder

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='mx-auto max-w-[1440px]'>
-      <div className='container'>Footer</div>
-    </footer>
-  );
-};
-export default Footer;
---
+++ src/modules/Header/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Logo = () => {
+  return (
+    <div className='relative h-[34px] w-[29px] xl:h-[67px] xl:w-[57px]'>
+      <Image src='/icons/logo.svg' alt='User image' fill />
+    </div>
+  );
+};
+
+export default Logo;
