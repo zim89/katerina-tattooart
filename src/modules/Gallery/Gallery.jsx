b79150e86ccaf0c5413ee6f6aaae918b85bb3e7d
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-09-20 21:46:50 +0200

    Added base select component

--- src/modules/Gallery/Gallery.jsx
+++ src/modules/Gallery/Gallery.jsx
@@ -1,5 +1,17 @@
+import PicturesSelect from './components/PicturesSelect';
+import styles from './styles/styles.module.css';
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <section className='section'>
+      <div className='container'>
+        <h2 className={`${styles.header} mb-4`}>Галерея робіт</h2>
+        <div className='flex justify-end'>
+          <PicturesSelect />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Gallery;
---
+++ src/modules/Gallery/components/PicturesSelect.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import clsx from 'clsx';
+import makeAnimated from 'react-select/animated';
+import Select, { components } from 'react-select';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+
+const options = [
+  { value: 'all', label: 'Усі' },
+  { value: 'legs', label: 'На нозі' },
+  { value: 'hands', label: 'На руці' },
+  { value: 'back', label: 'На спині' },
+  { value: 'body', label: 'На тулубі' },
+  { value: 'shoulders', label: 'На плечах' },
+  { value: 'neck', label: 'На шиї' },
+];
+
+const DropdownIndicator = (props) => {
+  const { menuIsOpen } = props.selectProps;
+
+  return (
+    <components.DropdownIndicator {...props}>
+      <ChevronDownIcon
+        className={clsx(
+          'absolute right-2.5 top-2 h-6 w-6 transition-transform',
+          menuIsOpen && '-scale-y-100'
+        )}
+      />
+    </components.DropdownIndicator>
+  );
+};
+
+const PicturesSelect = ({ className }) => {
+  return (
+    <Select
+      unstyled
+      defaultValue={options[2]}
+      options={options}
+      components={{ DropdownIndicator }}
+      className={className}
+      classNames={{
+        control: (state) =>
+          clsx(
+            'p-2 bg-white/[.14] rounded-lg max-w-max min-w-[137px] leading-[19px]',
+            state.menuIsOpen && 'rounded-b-none'
+          ),
+        menuList: () =>
+          'bg-white/[.14] px-2.5 pb-4 pt-1.5 space-y-4 rounded-b-lg',
+      }}
+    />
+  );
+};
+
+export default PicturesSelect;
