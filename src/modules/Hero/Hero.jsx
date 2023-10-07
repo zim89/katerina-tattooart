697cbdfd42ef773e41f2e2e4063cb02d89624b81
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-07 23:08:51 +0200

    Reverted changed with css modules

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,23 +1,22 @@
+import clsx from 'clsx';
+import styles from './styles/hero.module.css';
+
 const Hero = () => {
   return (
-    <section className='md:mb-7.5 mb-5 xl:mb-10'>
+    <section className={styles.hero}>
       <div className='container'>
-        <div className='bg-hero-sm relative h-[21.75rem] bg-[length:244px] bg-center bg-no-repeat'>
+        <div className={styles.wrap}>
           {/* Title */}
-          <div className='absolute -left-[4px] top-[148px] flex gap-[62px] md:left-5.5 md:top-[302px] md:gap-[137px] xl:left-[43px] xl:top-[355px] xl:gap-[209px]'>
-            <span className='font-nautigal text-[56px] font-normal leading-normal text-white md:text-[100px] xl:text-[180px]'>
-              Katerina
-            </span>
-            <span className='font-nautigal text-[56px] font-normal leading-normal text-white md:text-[100px] xl:text-[180px]'>
-              Tattooart
-            </span>
+          <div className={styles.titleWrap}>
+            <span className={styles.title}>Katerina</span>
+            <span className={styles.title}>Tattooart</span>
           </div>
 
           {/* Slogan */}
-          <p className='absolute left-2 top-[11px] text-[12px] font-normal leading-normal tracking-[0.96px] text-[#E0E0E0] text-opacity-40 md:left-[130px] md:top-4 md:text-[14px] md:tracking-[1.12px] xl:left-[280px] xl:top-6 xl:text-[20px] xl:tracking-[1.6px]'>
+          <p className={clsx(styles.slogan, styles.sloganLeft)}>
             Приношу біль для
           </p>
-          <p className='absolute right-0 top-[11px] text-[12px] font-normal leading-normal tracking-[0.96px] text-[#E0E0E0] text-opacity-40 md:right-[114px] md:top-4 md:text-[14px] md:tracking-[1.12px] xl:right-[275px] xl:top-6 xl:text-[20px] xl:tracking-[1.6px]'>
+          <p className={clsx(styles.slogan, styles.sloganRight)}>
             вашого задоволення
           </p>
         </div>
